9650194b22e47cc198785058b205d574c4a95fce
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-03-16 19:02:45 -0400

    added code files

---
+++ kn.cpp
@@ -0,0 +1,21 @@
+/*
+File name: kn.cpp
+Author: Evan Gertis
+Date: 03/16/2019
+Purpose: contains the function implementations used to solve the knights tour problem.
+*/
+
+#pragma once
+#include "pch.h"
+
+//Begin KnightsTour
+KN::KnightsTour::KnightsTour()
+{
+}
+//End KnightsTour
+
+//Begin ~KnightsTour
+KN::KnightsTour::~KnightsTour()
+{
+}
+//End ~KnightsTour
---
+++ kn.h
@@ -0,0 +1,25 @@
+#pragma once
+/*
+File: kn.h
+Author: Evan Gertis.
+Date: 03/16/2019
+Purpose: Holds defs, class, namespace, and memebers used in kn.cpp
+*/
+
+//Begin namespace
+namespace KN {
+
+	//Begin class
+	class KnightsTour
+	{
+	public:
+		KnightsTour();
+		~KnightsTour();
+
+	private:
+
+	};
+	//End class
+
+}
+//End namespace
